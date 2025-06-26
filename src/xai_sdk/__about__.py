17d560f8e80c5680a9703291af0ba5dc7b6bb6e2
Omar <od@x.ai>
2025-06-26 20:37:25 +0100

    Bump version from v1.0.0rc1 -> v1.0.0rc2 (#8)
    - Prepare to release v1.0.0rc2 of the SDK following the fix in #7

--- src/xai_sdk/__about__.py
+++ src/xai_sdk/__about__.py
@@ -3,4 +3,4 @@
 # To change the version, do so using `uv run hatch version <new-version>`
 # or `uv run hatch version <patch|minor|major>` to bump the patch/minor/major version respectively.
 # See https://hatch.pypa.io/latest/version/#updating for more details.
-__version__ = "1.0.0rc1"
+__version__ = "1.0.0rc2"
